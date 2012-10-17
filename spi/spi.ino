e1b87ce8d626db5d4efa7acdc1167fdebbd6a7d9
Marshal Leslie <mleslie09@wou.edu>
2012-10-17 15:33:37 -0700

    initial code for display

---
+++ spi/spi.ino
@@ -0,0 +1,20 @@
+#include <SPI.h>
+
+const int slaveSelectPin = 10;
+
+void setup() {
+    pinMode (slaveSelectPin, OUTPUT);
+    SPI.begin();
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setClockDivider(SPI_CLOCK_DIV128);
+}
+
+void loop() {
+    digitalWrite(slaveSelectPin,LOW);
+    SPI.transfer(1);
+    SPI.transfer(2);
+    SPI.transfer(3);
+    SPI.transfer(4);
+    digitalWrite(slaveSelectPin,HIGH);
+}
