ee74b631649bfc0d33f01ceccbbce68ea6e637a3
Marshal Leslie <mleslie09@wou.edu>
2012-10-17 15:06:14 -0700

    Infrared Code

---
+++ infrared.ino
@@ -0,0 +1,27 @@
+float aRead;
+float distance;
+float cm_distance;
+float in_distance;
+
+int analogPIN = 3;
+
+void setup()
+{
+ Serial.begin(9600); 
+}
+void loop()
+{
+  distance =  5.0 / 1023;
+  cm_distance = (distance * 0.42 ) - 2.21;
+  in_distance = cm_distance * 2.54;
+  aRead = analogRead(analogPIN);  
+  Serial.println(aRead);
+  
+  Serial.print("Distances in CM: ");
+  Serial.println(cm_distance);  
+  Serial.println();
+
+  Serial.print("Distances in IN: ");
+  Serial.println(in_distance);  
+  delay(1000);
+}
